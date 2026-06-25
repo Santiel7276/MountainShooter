7508ca6f42484e6e9e360fb571e44f210a01744e
Igor Rocha <igor.rocha.7276@gmail.com>
2026-06-25 02:27:31 -0300

    Creating Players and Enemies Moving Players and Enemies

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,3 +1,13 @@
+from code.Const import ENTITY_SPEED, WIN_WIDTH
+from code.Entity import Entity
+
+
 class Enemy(Entity):
-    def move(self) -> None:
-        pass
+    def __init__(self, name, position):
+        super().__init__(name, position)
+
+    def move(self, ):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.right <= 0:
+            self.rect.left = WIN_WIDTH
+
