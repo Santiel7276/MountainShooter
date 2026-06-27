2244bb3e77e76eb9fc36987da4e68cf81700ee76
Igor Rocha <igor.rocha.7276@gmail.com>
2026-06-27 16:22:34 -0300

    Fixing bugs.

--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,5 +1,6 @@
 from code.Const import WIN_WIDTH
 from code.Enemy import Enemy
+from code.EnemyShot import EnemyShot
 from code.Entity import Entity
 from code.PlayerShot import PlayerShot
 
@@ -9,19 +10,20 @@ class EntityMediator:
     @staticmethod
     def __verify_collision_window(ent: Entity):
         if isinstance(ent, Enemy):
-            if ent.rect.right < 0:
+            if ent.rect.right <= 0:
                 ent.health = 0
         if isinstance(ent, PlayerShot):
             if ent.rect.left >= WIN_WIDTH:
                 ent.health = 0
-        pass
+        if isinstance(ent, EnemyShot):
+            if ent.rect.right <= 0:
+                ent.health = 0
 
     @staticmethod
     def verify_collision(entity_list: list[Entity]):
         for i in range(len(entity_list)):
             test_entity = entity_list[i]
             EntityMediator.__verify_collision_window(test_entity)
-
 
     @staticmethod
     def verify_health(entity_list):
--- code/Player.py
+++ code/Player.py
@@ -7,7 +7,7 @@ from code.PlayerShot import PlayerShot
 
 
 class Player(Entity):
-    def __init__(self, name, position, ENTITY_SHOT_DELAY=None):
+    def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
         self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
